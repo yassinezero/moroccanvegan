fb3014e0440941086ab66bab56e0d844925704fc
Yassine Touri <yassinetouri2020@gmail.com>
2021-09-06 18:01:56 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+import requests
+username = 'moroccanvegan'
+token = '0324f5198b6e8c456e1536ae3c4b7c5ef56386e6'
+
+response = requests.get(
+    'https://www.pythonanywhere.com/api/v0/user/{username}/cpu/'.format(
+        username=username
+    ),
+    headers={'Authorization': 'Token {token}'.format(token=token)}
+)
+if response.status_code == 200:
+    print('CPU quota info:')
+    print(response.content)
+else:
+    print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
